83c950925e8ca8d1a8409b6cdaff726cdc0aecad
jsnkan <jasonkanavas@outlook.com>
2025-03-06 22:55:35 +0200

    initial commit

---
+++ lcd128_32_test/lcd128_32_test.ino
@@ -0,0 +1,23 @@
+/*
+   lcd128_32 Software IIC subroutine, applicable toarduino, esp32, raspberry pi pico
+*/
+#include "lcd128_32_io.h"
+
+//Create lCD128*32 pin，sda--->21， scl--->22
+lcd lcd(21, 22);
+
+void setup() {
+  lcd.Init(); //initialize
+  lcd.Clear();  //clear
+}
+
+void loop() {
+  lcd.Cursor(0, 4); //Set display position
+  lcd.Display("KEYESTUDIO"); //Setting the display
+  lcd.Cursor(1, 0);
+  lcd.Display("ABCDEFGHIJKLMNOPQR");
+  lcd.Cursor(2, 0);
+  lcd.Display("123456789+-*/<>=$@");
+  lcd.Cursor(3, 0);
+  lcd.Display("%^&(){}:;'|?,.~\\[]");
+}
